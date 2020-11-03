c597ed7bec2a12ceca86c948d5e9b9ce61838b5b
keval718 <kevalvirat@gmail.com>
2020-11-03 01:32:10 -0500

    Icons added

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,6 +4,13 @@ import CreateIcon from '@material-ui/icons/Create';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import SidebarOption from "./SidebarOption";
 import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import AppsIcon from '@material-ui/icons/Apps';
+import FileCopyIcon from '@material-ui/icons/FileCopy';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 function Sidebar() {
     return (
@@ -23,6 +30,16 @@ function Sidebar() {
             </div>
             <SidebarOption Icon={InsertCommentIcon} title="Threads" />
             <SidebarOption title="Youtube" />
+            <SidebarOption Icon={InboxIcon} title="Mention & reactions" />
+            <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+            <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+            <SidebarOption Icon={PeopleAltIcon} title="People" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+            <SidebarOption Icon={AppsIcon} title="Apps" />
+            <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+
+        
         </div>
     )
 }
